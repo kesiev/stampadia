ab467d2ba40d63bfef1322683408a006f52299aa
KesieV <kesiev@gmail.com>
2021-08-05 17:03:23 +0200

    :hammer: Added a new codec section

--- tools/build-codec.sh
+++ tools/build-codec.sh
@@ -4,7 +4,7 @@ precategory=""
 echo "# Chronicles of Stampadia - Codec"
 echo ""
 echo "<div align=center style='border:2px solid red;padding:5px'><b>Spoiler alert!</b> This codec contains a list and a description of all of the Chronicles of Stampadia contents. If you don't want to spoil the fun, <b>stop reading this now</b>.</div>"
-for group in Events Stuff
+for group in Events Heroes Stuff
 do
 	count="$(grep -h "\[CODEC-$group\]" ../database/*|wc -l|sed "s/ //g")"
 	echo ""
