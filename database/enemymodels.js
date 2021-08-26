8b34351fa509ed0f048fed8e69ac8502699482d4
KesieV <kesiev@gmail.com>
2021-08-26 08:53:55 +0200

    :video_game: Added enemy variants

--- database/enemymodels.js
+++ database/enemymodels.js
@@ -5,25 +5,30 @@ function loadEnemyModels() {
 		// Level 0
 		{
 			skills:[
-				["ATK -1\nRNG 1","Move\n-2"]
+				["ATK -1\nRNG 1","Move\n-2"],
+				["ATK -1\nRNG 2","Move\n-3"]
 			]
 		},
 		// Level 1
 		{
 			skills:[
-				["DEF -4","Move\n-1"]
+				["DEF -4","Move\n-1"],
+				["DEF -3","Move\n-2"]
 			]
 		},
 		// Level 2
 		{
 			skills:[
-				["DEF -3","ATK -3\nRNG 1"]
+				["DEF -3","ATK -3\nRNG 1"],
+				["DEF -4","ATK -3\nRNG 2"],
+				["DEF -3","ATK -3\nRNG 2"],
 			]
 		},
 		// Level 3
 		{
 			skills:[
-				["ATK\nRNG 1","ATK -2\nRNG 1"]
+				["ATK\nRNG 1","ATK -2\nRNG 1"],
+				["ATK\nRNG 1","ATK -2\nRNG 2"]
 			]
 		}
 	];
