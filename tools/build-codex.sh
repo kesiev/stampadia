e284f22dd567119bc28f16f058d86f2fe53ef789
KesieV <kesiev@gmail.com>
2021-10-11 09:06:18 +0200

    :hammer: Now ignoring duplicated items

--- tools/build-codex.sh
+++ tools/build-codex.sh
@@ -6,11 +6,11 @@ echo ""
 echo "<div align=center style='border:2px solid red;padding:5px'><b>Spoiler alert!</b> This codex contains a list and a description of all of the Chronicles of Stampadia contents. If you don't want to spoil the fun, <b>stop reading this now</b>.</div>"
 for group in Events Heroes Stuff Keywords Generator
 do
-	count="$(grep -h "\[CODEX-$group\]" ../database/* ../js/*|wc -l|sed "s/ //g")"
+	count="$(grep -h "\[CODEX-$group\]" ../database/* ../js/*|sed "s/.*\[CODEX-$group\] //;s/ ([0-9]*)//g"|sort|uniq|wc -l|sed "s/ //g")"
 	echo ""
 	echo "## $group ($count items)"
 	echo ""
-	for line in $(grep -h "\[CODEX-$group\]" ../database/*  ../js/*|sed "s/.*\[CODEX-$group\] //"|sort)
+	for line in $(grep -h "\[CODEX-$group\]" ../database/*  ../js/*|sed "s/.*\[CODEX-$group\] //;s/ ([0-9]*)//g"|sort|uniq)
 	do
 		line="$(echo "$line"|sed "s/\",.*$//")"
 		category="$(echo "$line"|sed "s/ -.*//")"
