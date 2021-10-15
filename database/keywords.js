9859dd4ea671816b0b858175bfefda29bf248ebd
KesieV <kesiev@gmail.com>
2021-10-15 09:05:37 +0200

    :video_game: Added keywords sets, Beastcrafters cards

--- database/keywords.js
+++ database/keywords.js
@@ -1,51 +1,73 @@
 /* exported loadKeywords */
 
 function loadKeywords() {
-	return [
-		{
-			// [CODEX-Keywords] Keyword - Computer: Part of the The End Of Stampadia storyline.
-			id:"computer",
-			label:"Computer"
-		},
-		{
-			// [CODEX-Keywords] Keyword - Irony: Part of the The End Of Stampadia storyline.
-			id:"electricity",
-			label:"Electricity"
-		},
-		{
-			// [CODEX-Keywords] Keyword - Spaceship: Part of the The End Of Stampadia storyline.
-			id:"spaceship",
-			label:"Spaceship"
-		},
-		{
-			// [CODEX-Keywords] Keyword - City: Part of the The End Of Stampadia storyline.
-			id:"city",
-			label:"City"
-		},
-		{
-			// [CODEX-Keywords] Keyword - Ink: Part of the The End Of Stampadia storyline.
-			id:"ink",
-			label:"Ink"
-		},
-		{
-			// [CODEX-Keywords] Keyword - Press: Part of the The End Of Stampadia storyline.
-			id:"press",
-			label:"Press"
-		},
-		{
-			// [CODEX-Keywords] Keyword - End: Part of the The End Of Stampadia storyline.
-			id:"end",
-			label:"End"
-		},
-		{
-			// [CODEX-Keywords] Keyword - Luck: It may bring you good luck during your adventures...
-			id:"luck",
-			label:"Luck"
-		},
-		{
-			// [CODEX-Keywords] Keyword - Unluck: It may bring you bad luck during your adventures...
-			id:"unluck",
-			label:"Unluck"
-		}
-	];
+	return {
+		list:[
+			{
+				// [CODEX-Keywords] Keyword - Computer: Part of the The End Of Stampadia storyline.
+				id:"computer",
+				label:"Computer"
+			},
+			{
+				// [CODEX-Keywords] Keyword - Irony: Part of the The End Of Stampadia storyline.
+				id:"electricity",
+				label:"Electricity"
+			},
+			{
+				// [CODEX-Keywords] Keyword - Spaceship: Part of the The End Of Stampadia storyline.
+				id:"spaceship",
+				label:"Spaceship"
+			},
+			{
+				// [CODEX-Keywords] Keyword - City: Part of the The End Of Stampadia storyline.
+				id:"city",
+				label:"City"
+			},
+			{
+				// [CODEX-Keywords] Keyword - Ink: Part of the The End Of Stampadia storyline.
+				id:"ink",
+				label:"Ink"
+			},
+			{
+				// [CODEX-Keywords] Keyword - Press: Part of the The End Of Stampadia storyline.
+				id:"press",
+				label:"Press"
+			},
+			{
+				// [CODEX-Keywords] Keyword - End: Part of the The End Of Stampadia storyline.
+				id:"end",
+				label:"End"
+			},
+			{
+				// [CODEX-Keywords] Keyword - Luck: It may bring you good luck during your adventures...
+				id:"luck",
+				label:"Luck"
+			},
+			{
+				// [CODEX-Keywords] Keyword - Unluck: It may bring you bad luck during your adventures...
+				id:"unluck",
+				label:"Unluck"
+			},
+			{ id:"golem-1", label:"Golem 1" },{ id:"golem-2", label:"Golem 2" },{ id:"golem-3", label:"Golem 3" },
+			{ id:"manticore-1", label:"Manticore 1" },{ id:"manticore-2", label:"Manticore 2" },{ id:"manticore-3", label:"Manticore 3" },
+			{ id:"hellhound-1", label:"Hellhound 1" },{ id:"hellhound-2", label:"Hellhound 2" },{ id:"hellhound-3", label:"Hellhound 3" },
+		],
+		sets:[
+			{
+				// [CODEX-Keywords] Keyword set - Golem: Beastcrafters collectable cards from 1 to 3.
+				id:"golemSet",
+				label:"Golem"
+			},
+			{
+				// [CODEX-Keywords] Keyword set - Manticore: Beastcrafters collectable cards from 1 to 3.
+				id:"manticoreSet",
+				label:"Manticore"
+			},
+			{
+				// [CODEX-Keywords] Keyword set - Hellhound: Beastcrafters collectable cards from 1 to 3.
+				id:"hellhoundSet",
+				label:"Hellhound"
+			}
+		]
+	};
 }
